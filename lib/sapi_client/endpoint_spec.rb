1eb946c7dc4811629539d4593e1e77ec09d1e66c
Ian Dickinson <ian.dickinson@epimorphics.com>
2019-01-21 12:25:28 +0000

    SAPI specification is a series of YAML documents in one file, so load the file first then parse the stream

--- lib/sapi_client/endpoint_spec.rb
+++ lib/sapi_client/endpoint_spec.rb
@@ -4,22 +4,25 @@ module SapiClient
   # Encapsulates the specification of a collection of SAPI endpoints, and some
   # other features that are useful to the server but not to the client
   class EndpointSpec
-    def initialize(spec_file_name)
-      @specification = YAML.load_file(spec_file_name)
-    rescue Errno::ENOENT
-      raise(SapiClient::Error, "No such specification file: #{spec_file_name}")
+    def initialize(base_url, spec_file_name)
+      unless File.exist?(spec_file_name)
+        raise(SapiClient::Error, "No such specification: #{spec_file_name}")
+      end
+
+      @specification = YAML.load_stream(File.read(spec_file_name))
+      @base_url = base_url
     end
 
     attr_reader :specification
+    attr_reader :base_url
 
     def endpoints
       endpoint_specs = specification.select do |spec|
-        byebug
         type = spec['type']
         type && SapiEndpoint::ENDPOINT_TYPES.include?(type)
       end
 
-      endpoint_specs.map { |spec| SapiEndpoint.new(spec) }
+      endpoint_specs.map { |spec| SapiEndpoint.new(base_url, spec) }
     end
   end
 end
--- test/sapi_client/endpoint_spec_test.rb
+++ test/sapi_client/endpoint_spec_test.rb
@@ -6,26 +6,29 @@ require 'sapi_client'
 module SapiClient
   class EndpointSpecTest < Minitest::Test
     describe 'EndpointSpec' do
+      let(:base_url) { 'http://foo.bar' }
+      let(:spec_file) { 'test/fixtures/endpointSpecs/establishment.yaml' }
+
       describe '#initialize' do
         it 'should be initialized by loading a YAML spec file' do
-          es = SapiClient::EndpointSpec.new('test/fixtures/endpointSpecs/establishment.yaml')
-          es.specification.must_be_kind_of Hash
+          es = SapiClient::EndpointSpec.new(base_url, spec_file)
+          es.specification.must_be_kind_of Array
         end
 
         it 'should raise if the path to the endpoint specification cannot be found' do
           lambda {
-            SapiClient::EndpointSpec.new('test/fixtures/endpointSpecs/establishment.womble')
+            SapiClient::EndpointSpec.new(base_url, 'test/fixtures/endpointSpecs/womble.yaml')
           }.must_raise(SapiClient::Error)
         end
       end
 
       describe '#endpoints' do
         it 'should return an array of all of the endpoints in the specification' do
-          es = SapiClient::EndpointSpec.new('test/fixtures/endpointSpecs/establishment.yaml')
+          es = SapiClient::EndpointSpec.new(base_url, spec_file)
           eps = es.endpoints
 
           eps.length.must_equal 2
-          eps.first.must_be_kind_of(SapiClient::Endpoint)
+          eps.first.must_be_kind_of(SapiClient::SapiEndpoint)
         end
       end
     end
