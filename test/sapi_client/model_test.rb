ddef789bcaa9ce5fbee31609c725ca61ff5aad9d
Alex Tucker <alex.tucker@epimorphics.com>
2025-12-09 17:28:35 +0000

    Placate rubocop

--- test/sapi_client/model_test.rb
+++ test/sapi_client/model_test.rb
@@ -1,33 +1,29 @@
 # frozen_string_literal: true
 
-# frozen_string_literal: true
-
 require 'test_helper'
 require 'sapi_client'
 
 module SapiClient
-
   class ModelTest < Minitest::Test
     describe 'regulated product applications model' do
       before do
         @app = SapiClient::Application.new(
           'https://fsa-rpa-dev.epimorphics.net',
-          'test/fixtures/rp-applications/application.yaml')
+          'test/fixtures/rp-applications/application.yaml'
+        )
       end
 
       describe 'application with missing property' do
-        it 'shouldn''t raise missing method exception on optional, missing property' do
+        it 'shouldn\'t raise missing method exception on optional, missing property' do
           inst = @app.instance
           VCR.use_cassette('model_test.application_missing_property') do
-            item = inst.application_item({:id => 'RP-3099'})[0]
+            item = inst.application_item({ id: 'RP-3099' })[0]
             _(item.notation).must_equal 'RP-3099'
             _(item.path_first('marketingName')).must_be_nil
             _(item.marketingName).must_be_nil
           end
         end
       end
-
     end
   end
-
 end
