6cee24651636f23ab9628418ecb46590bf041bbc
Bogdan Marc <marc.bogdan96@yahoo.com>
2026-03-11 16:01:54 +0000

    Added to_underscore method to prevent rails require

--- lib/sapi_client/application.rb
+++ lib/sapi_client/application.rb
@@ -103,5 +103,13 @@ module SapiClient
         inst.get_hierarchy(endpoint_url, options, scheme)
       end
     end
+    # Helper method for parsing model spec file
+    def to_underscore(string)
+      string.gsub('::', '/')
+            .gsub(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2')
+            .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+            .tr('-', '_')
+            .downcase
+    end
   end
 end
