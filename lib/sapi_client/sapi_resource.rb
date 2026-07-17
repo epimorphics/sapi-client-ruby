840d943b100757f8cdd8e1b8eeeb435821f30544
Bogdan Marc <marc.bogdan96@yahoo.com>
2026-07-17 10:44:02 +0100

    Use new attr_accessor in helper method

--- lib/sapi_client/sapi_resource.rb
+++ lib/sapi_client/sapi_resource.rb
@@ -258,12 +258,13 @@ module SapiClient
       typ.is_a?(String) ? typ : typ['@id']
     end
 
-    # Helper method to find if property is in PARSED_MODEL_SPEC for the corresponding type
+    # Helper method to find if property is in parsed_model_spec for the corresponding type
     def property_in_model_spec?(property)
+      return false unless (spec = self.class.parsed_model_spec)
+
       types&.any? do |typ|
         full_type = type_to_string(typ)
-        Application::PARSED_MODEL_SPEC.key?(full_type) &&
-          Application::PARSED_MODEL_SPEC[full_type].key?(property.to_s)
+        spec.key?(full_type) && spec[full_type].key?(property.to_s)
       end
     end
   end
